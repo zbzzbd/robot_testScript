310f1bd77a18497797fffc4c1516ea685742ee62
zhangbignzhen <zhangbingzhen@chinarun.com>
2015-12-24 10:02:47 +0800

    增加ui自动化脚本

---
+++ auto_ui/dev_conf.py
@@ -0,0 +1,7 @@
+__all__ = ['base_url','db_url','db_port','db_user','db_password','db_name']
+base_url='http://localhost:8080'
+db_url='localhost'
+db_port='3306'
+db_user='root'
+db_password='root'
+db_name='achilles'
